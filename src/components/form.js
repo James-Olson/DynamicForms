09f20b3272a03d66192628d5b408927a0e80891f
Jim Olson <jim.olson.456@gmail.com>
2024-04-15 22:51:37 -0500

    WorkingOnForm

--- src/components/form.js
+++ src/components/form.js
@@ -7,9 +7,16 @@ function Form() {
     Value(event.target.value)
   };
 
-  let handleSubmit = (event) => {
+  let Submit = (event) => {
     console.log(InputValue)
   };
 }
+
+return Form (
+  <form Enter={Submit}>
+      <input value={InputValue} onChange={handleChange} />
+    <button type="submit">Enter</button>
+  </form>
+);
 
 export default Form
